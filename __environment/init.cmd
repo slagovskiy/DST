bd1f86504499fd3afc2386a12162d55762da2fad
Sergey Lagovskiy <slagovskiy@gmail.com>
2023-03-02 16:53:28 +0700

    ml-4

--- __environment/init.cmd
+++ __environment/init.cmd
@@ -1,5 +1,5 @@
 mkdir venv
-compact /c /i /q /f /s:venv
+rem compact /c /i /q /f /s:venv
 virtualenv venv
 venv\Scripts\python.exe -m pip install --upgrade pip
 venv\Scripts\python.exe -m pip install --upgrade -r requirements.txt
